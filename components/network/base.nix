8925a8b20a141f826dd008f65a4c072e63a7c0ff
kxxt <rsworktech@outlook.com>
2023-06-03 17:28:29 +0800

    feat: basic network settings

---
+++ components/network/base.nix
@@ -0,0 +1,4 @@
+{
+  # Use Networkmanager
+  networking.networkmanager.enable = true;
+}
---
+++ components/network/proxy-clash.nix
@@ -0,0 +1,4 @@
+{
+  networking.proxy.default = "http://127.0.0.1:7890";
+  networking.proxy.noProxy = "127.0.00.1,localost,internal.domain";
+}
