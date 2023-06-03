b49d98f2891af7a5c8ea7cebf87249020dd4d565
kxxt <rsworktech@outlook.com>
2023-06-03 17:28:13 +0800

    feat: intel/nvidia hybrid graphics

---
+++ components/drivers/nvidia/intel-nvidia-laptop.nix
@@ -0,0 +1,12 @@
+{ config, ... }: {
+  # Nvidia Configuration
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    reverseSync.enable = true;
+  };
+}
