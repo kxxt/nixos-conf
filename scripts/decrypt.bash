524bf5b57e167735a6e7c2132db0f6fcbc29e8a4
kxxt <rsworktech@outlook.com>
2023-06-03 20:04:09 +0800

    fix: wrong outdir for decryption

--- scripts/decrypt.bash
+++ scripts/decrypt.bash
@@ -6,7 +6,7 @@ PRIVATE_DIR="$SCRIPT_DIR/../private"
 echo "Decrypting secrets... Please make sure the private key is available!"
 
 find "$PRIVATE_DIR" -type f -name '*.gpg' \
-    -exec gpg --batch --yes --use-embedded-filename --decrypt {} \; \
-    -exec echo "Decrypted {}" \; \
+    -exec bash -c 'gpg --batch --yes --output "${1%.gpg}" --decrypt "$1"' bash {} \; \
+    -exec echo "Decrypted {}" \;
 
 echo "Success!"
