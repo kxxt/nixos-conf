39c286263fe23e6956100fa8c9f2f3de9b5fe2cf
kxxt <rsworktech@outlook.com>
2023-06-04 08:41:43 +0800

    ime: chinese support

--- components/services/ime-fcitx.nix
+++ components/services/ime-fcitx.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   i18n.inputMethod.enabled = "fcitx5";
-  i18n.inputMethod.fcitx5.addons = [
-    # TODO
+  i18n.inputMethod.fcitx5.addons = with pkgs; [
+    fcitx5-chinese-addons
+    fcitx5-gtk
   ];
 }
