0f600624685948e4414a7c43c700ef5e7fbad167
kxxt <rsworktech@outlook.com>
2023-06-04 09:48:37 +0800

    KDE: refactor & add plasma-thunderblot

--- components/display/kde.nix
+++ components/display/kde.nix
@@ -12,9 +12,11 @@
   # https://nixos.wiki/wiki/KDE#GTK_themes_are_not_applied_in_Wayland_applications
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [
-    libsForQt5.kcolorchooser
+  ] ++ (with pkgs.libsForQt5; [
+    kdeconnect-kde
+    kcolorchooser
     plasma-pa
-    libsForQt5.kdeconnect-kde
-    libsForQt5.qtstyleplugin-kvantum
-  ];
+    qtstyleplugin-kvantum
+    plasma-thunderbolt
+  ]);
 }
